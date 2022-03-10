e6d5ccd9d71098f1527794cbc833d96bd8f609d7
iggyvilla <iggyvilla@icloud.com>
2022-03-11 02:52:26 +0800

    fixed attendance response

--- tapapi/plugins/attendance.py
+++ tapapi/plugins/attendance.py
@@ -1,6 +1,7 @@
 from utils.responses import PluginResponse
 from utils.parse_payload import TapAPIRequestPayload
 from plugins.plugin_utils import attendance_utils
+from datetime import datetime
 import psycopg2.errors
 
 
@@ -10,6 +11,7 @@ event_name = "attendance"
 def run(jwt_decoded: dict, payload_data: TapAPIRequestPayload, args, conn) -> PluginResponse:
     try:
         attendance_utils.insert_into_attendance_logs(jwt_decoded["name"], payload_data.uid, conn)
-        return PluginResponse(200, payload={"msg": "ok"})
+        # Format time to Day (name) Day (number) Hour (24-hour):minute
+        return PluginResponse(200, payload={"msg": "ok", "data": {"time_now": datetime.now().strftime("%a %d %H:%I")}})
     except psycopg2.errors.Error:
         return PluginResponse(500, payload={"msg": "internal server database error"})
